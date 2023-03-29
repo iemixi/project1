892c872b0fc9469f2842408abdca746c1fbe9b48
iemixi <iemix1976@yandex.ru>
2023-03-29 13:28:46 +0300

    initial commit

---
+++ src/main/java/ru/netology/project1/SalesManager.java
@@ -0,0 +1,19 @@
+package ru.netology.project1;
+
+public class SalesManager {
+    protected int[] sales;
+
+    public SalesManager(int[] sales) {
+        this.sales = sales;
+    }
+
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+        return max;
+    }
+}
