78f2e405dd88ad8f5fd0035c1f26387112de1643
iemixi <iemix1976@yandex.ru>
2023-03-29 13:29:13 +0300

    добавил пример использования класса и описание проекта

---
+++ src/main/java/ru/netology/project1/Main.java
@@ -0,0 +1,10 @@
+package ru.netology.project1;
+
+public class Main {
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new int[]{1, 2, 3, 4});
+        int max = salesManager.max();
+
+        System.out.println(max);
+    }
+}
